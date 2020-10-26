1b2c70142a5c07af472fa5c8d01139c530f06618
ancalex <andrei.erdei@gmail.com>
2020-10-26 19:57:56 +0200

    Update HST_LED_Art.ino

--- the LED Art/HST_LED_Art.ino
+++ the LED Art/HST_LED_Art.ino
@@ -28,8 +28,8 @@ const byte m_leds[128] = {
 
 const byte hst_cell_offset[] = {0, 1, 9, 17, 25, 24, 16, 8, 0, 1, 9, 17, 25, 24, 16, 8};
 const byte hst_cell_origin[] = {0, 2, 4, 6, 32, 34, 36, 38, 64, 66, 68, 70, 96, 98, 100, 102};
-byte hst_cell_position[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-byte animation_steps[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+byte hst_cell_position[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //array for intermediary hst positions
+byte animation_steps[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //how many animation steps between startand end position
 byte orig;
 byte pos;
 byte modelNumber = 0;
@@ -64,7 +64,7 @@ void setup()
 	Serial.println("FastLed Setup done");
 	fill_solid( leds, NUM_LEDS, CRGB(0,0,0));
 
-	for (int i = 0; i < 16; i++) {      // homing :)
+	for (int i = 0; i < 16; i++) {      // homing :) Display 16 HST in zero position
 		int origin = hst_cell_origin[i];
 		for (int j = 0; j < 8; j++) {
 			int offset = hst_cell_offset[j];
@@ -85,34 +85,34 @@ void setup()
 void loop()
 {
 	for (int m = 0; m < 16; m++) {
-		int step = pgm_read_byte(&HSTM[modelNumber][m]) - hst_cell_position[m];
-		if (step >= 0 ) {
-			animation_steps[m] = 2 * step;
+		int step = pgm_read_byte(&HSTM[modelNumber][m]) - hst_cell_position[m]; //how many steps from start to end position
+ 		if (step >= 0 ) {
+			animation_steps[m] = 2 * step; // 0-1, 0-2, 0-3, 1-2, 1-3, 2-3 positions
 		}
 		else {
-			animation_steps[m] = 2 * (4 + step);
+			animation_steps[m] = 2 * (4 + step); //3-0, 3-1, 3-2, 2-1, 2-0, 1-0
 		}
 		Serial.print(animation_steps[m]);Serial.print("-");
 	}
 	Serial.println();
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 8; i++) { //8 steps of animation one 360 degrees rotation
 		while (colorCurrent1 != colorTarget) {
 			EVERY_N_MILLISECONDS(blendRate) {
-				colorCurrent1 = blend(colorStart, colorTarget, k);
-				colorCurrent2 = blend(colorTarget, colorStart, k);
-				for (int j = 0; j < 16; j++) {
-					if (animation_steps[j] > 0) {
+				colorCurrent1 = blend(colorStart, colorTarget, k); //color of one end of the triangle
+				colorCurrent2 = blend(colorTarget, colorStart, k); //other end of the triangle
+				for (int j = 0; j < 16; j++) { //animate all hst
+					if (animation_steps[j] > 0) { // but only if there are any remaining steps for a hst
 					orig = hst_cell_origin[j];
-					leds[m_leds[orig + hst_cell_offset[i + (hst_cell_position[j] * 2)]]] = colorCurrent1;
-					leds[m_leds[orig + hst_cell_offset[i + 4 + (hst_cell_position[j] * 2)]]] = colorCurrent2;
+					leds[m_leds[orig + hst_cell_offset[i + (hst_cell_position[j] * 2)]]] = colorCurrent1; //color of one end of the triangle
+					leds[m_leds[orig + hst_cell_offset[i + 4 + (hst_cell_position[j] * 2)]]] = colorCurrent2; //other end of the triangle
 					//
 					}
 				}
-				k+=5;
+				k+=5; //animation speed, is working only with 1 and 5 !!!
 				FastLED.show();  // update the display
 			}
 		}
-		for (int n = 0; n < 16; n++) {
+		for (int n = 0; n < 16; n++) { //one step less after an iteration
 		if (animation_steps[n] > 0) {
 			animation_steps[n]--;
 		}
@@ -122,7 +122,7 @@ void loop()
 		colorCurrent2 = colorTarget;
 	}
 	for (int n = 0; n < 16; n++) {
-	hst_cell_position[n] = pgm_read_byte(&HSTM[modelNumber][n]);
+	hst_cell_position[n] = pgm_read_byte(&HSTM[modelNumber][n]); // save the actual model positions
 	}
 	modelNumber++;
 	if (modelNumber == 73) {modelNumber = 0;}
